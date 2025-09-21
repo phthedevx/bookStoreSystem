5a447d068becd4f3d9a149f346aea3eb050d1cf2
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-09-21 19:18:27 -0300

    feat: adicionar dados iniciais nas classes Autor, Livro e Biblioteca

--- src/main/java/com/phthedev/bookStoreSystem/Model/Autor.java
+++ src/main/java/com/phthedev/bookStoreSystem/Model/Autor.java
@@ -4,5 +4,37 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public class Autor {
+    private UUID id;
+    private String nome;
+    private LocalDate dataNascimento;
 
+    public Autor(UUID id, String nome, LocalDate dataNascimento ) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/main/java/com/phthedev/bookStoreSystem/Model/Livro.java
+++ src/main/java/com/phthedev/bookStoreSystem/Model/Livro.java
@@ -1,4 +1,70 @@
 package com.phthedev.bookStoreSystem.Model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Livro {
+    private UUID id;
+    private String titulo;
+    private Autor autor;
+    private boolean disponivel;
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataAtualizacao;
+
+    public Livro(UUID id, String titulo, Autor autor, boolean disponivel, LocalDateTime dataCadastro, LocalDateTime dataAtualizacao) {
+        this.id = id;
+        this.disponivel = disponivel;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataCadastro = dataCadastro;
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public LocalDateTime getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
 }
